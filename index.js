7ca2f0250aff5f781a6b8376d7973f6a061dd378
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-01-07 11:34:43 +0100

    initial

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+const path = require("path");
+const request = require('request-promise-native');
+
+
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+app.set('view engine', "ejs");
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/src/public')));
+
+
+//home
+app.get("/", function(req, res) {
+    res.render(__dirname + "/src/index.ejs");
+})
+
+
+
+//about
+app.get("/about", function(req, res) {
+    res.render(__dirname + "/src/about.ejs");
+})
+//deals
+app.get("/deals", function(req, res) {
+    res.render(__dirname + "/src/deals.ejs");
+})
+
+//reservation
+app.get("/reservation", function(req, res) {
+    res.render(__dirname + "/src/reservation.ejs");
+})
+app.get('/ass', function (req, res) {
+    res.render(__dirname + "/src/company.ejs");
+});
+
+app.get('/companies', (req, res) => {
+    const municipality = req.query.municipality;
+    const page = req.query.page;
+  
+    const options = {
+      method: 'GET',
+      uri: 'https://data.brreg.no/enhetsregisteret/api/enheter',
+      qs: {
+        'postadresse.poststed': municipality,
+        'page': page,
+        'size': 15
+      },
+      json: true
+    };
+  
+    request(options)
+      .then(response => {
+        console.log(response._embedded.enheter[0].navn);
+        res.send(response);
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send({error: 'An error occurred while retrieving the companies'});
+      });
+  });
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function(req, res) {
+    console.log("Server is running successfully");
+});
